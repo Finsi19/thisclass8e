9ccfce9f21b8dd9de0a76d0ed5b6679f22172070
Finsi19 <144135220+Finsi19@users.noreply.github.com>
2023-10-21 23:14:25 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+    // toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top <offset + height) {
+            // active navbar
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+            });
+            // active sec and nav
+            sec.classList.add('show-animate');
+        }
+        else{
+        sec.classList.remove('show-animate');
+        }
+    });
+
+    // sticky header
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    // animation footer on scroll
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight
+    );
+}
+
+// animasi text
+
+
+// ubah gambar about
+
+// var about = document.getElementById("about");
+// var about1 = "/images/about.jpg"; // Gambar awal
+// var about2 = "/images/about2.jpg"; // Gambar yang akan diganti saat diklik
+// var isAbout = true;
+
+// about.addEventListener("click", function() {
+//   if (isAbout) {
+//     about.src = about2;
+//     isAbout = false;
+//   } else {
+//     about.src = about1;
+//     isAbout = true;
+//   }
+// });
+
